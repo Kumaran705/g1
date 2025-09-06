37a43a4f0ff1e5afcc891d89e4ad9f82d9e8ffd0
Kumaran705 <btechece230914@smvec.ac.in>
2025-09-06 21:20:07 +0530

    Add files via upload

---
+++ Bankai.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Bankai {
+     public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);     
+ 
+      int a=sc.nextInt();
+      boolean nothing=true;
+      if(a<=1)
+          nothing=false;
+      else{
+           for(int i=2;i<=a/2;i++){
+               if(a%i==0)
+                    nothing=false;
+           }
+      }
+      if(nothing)
+             System.out.println("True");
+      else
+             System.out.println("False");    
+      }
+}
